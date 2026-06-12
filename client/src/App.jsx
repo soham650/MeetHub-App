7e22e0a04883aa47241b75d5211087f04da44477
Soham Jamkar <sohamjamkar54@gmail.com>
2026-06-12 20:35:08 +0530

    Fix Room import casing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Room from './pages/Room';
+import Room from './pages/room';
 
 function App() {
   return (
